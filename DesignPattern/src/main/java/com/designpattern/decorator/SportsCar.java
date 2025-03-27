05ba80580f4d89baa0726ffb746e3b53742fa7f0
Thùy Nhi IT <21054781.nhi@student.iuh.edu.vn>
2025-03-27 20:19:22 +0700

    decorator

---
+++ DesignPattern/src/main/java/com/designpattern/decorator/SportsCar.java
@@ -0,0 +1,12 @@
+package com.designpattern.decorator;
+
+public class SportsCar extends CarDecorator {
+    public SportsCar(Car c) {
+        super(c);
+    }
+    @Override
+    public void assemble(){
+        car.assemble();
+        System.out.print(" Adding features of Sports Car.");
+    }
+}
